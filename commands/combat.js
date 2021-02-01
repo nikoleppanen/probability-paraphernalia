9bf430eb2b8af6b8864d272280be026e2ebfde43
nikoleppanen <niko_leppanen@hotmail.com>
2021-02-01 10:16:53 +0200

    Add command manager & commands

---
+++ commands/combat.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'combat',
+	description: 'Test versus appropriate skill, includes critical hits and fumbles',
+	args: true,
+	execute(message, args) {
+		message.channel.send(`Not implemented yet!`);
+	},
+};
